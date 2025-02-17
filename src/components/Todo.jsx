58b1c326e1f6b03571ff70a41a86e5bf74e84a92
Sanjay6911 <sanjayvj691@gmail.com>
2025-02-17 23:31:00 +0530

    updated files

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -53,7 +53,7 @@ const Todo = () => {
             </div>
             <div className='flex items-center my-7 bg-gray-200 rounded-full'>
                 <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg [@media(max-width:400px)]:text-sm font-medium cursor-pointer'>ADD +</button>
             </div>
             <div>
                 {todoList.map((item,index) => {
